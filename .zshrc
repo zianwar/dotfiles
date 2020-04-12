18d5f56bb543e8c12c85a602b4b928b765da54e6
Anwar Ziani <anwaar.ziani@gmail.com>
2020-04-11 21:38:33 -0700

    Update dotfiles

--- .zshrc
+++ .zshrc
@@ -1,15 +1,19 @@
 # Path to oh-my-zsh installation.
+
 export DOTFILES=$HOME/.dotfiles
-export ZSH=$HOME/.dotfiles/oh-my-zsh
+export ZSH="$DOTFILES/oh-my-zsh"
+
+# Load the shell dotfiles, and then some:
+# * ~/.extra can be used for other settings you don’t want to commit.
+for file in $DOTFILES/.{exports,aliases,functions,extra}; do
+	[ -r "$file" ] && [ -f "$file" ] && source "$file";
+done;
+unset file;
 
 # oh-my-zsh variables
 ZSH_THEME="ep"
 CASE_SENSITIVE="false"
 DISABLE_AUTO_UPDATE="true"
-
-source $DOTFILES/aliases.zsh
-source $DOTFILES/functions.zsh
-source $DOTFILES/secrets.zsh
 
 # oh-my-zsh plugins
 plugins=(git osx zsh-completions history zsh-syntax-highlighting colorize docker z command-not-found gitignore golang)
@@ -17,44 +21,9 @@ source $ZSH/oh-my-zsh.sh
 eval $(/usr/libexec/path_helper -s)
 autoload -U compinit && compinit
 
-# User configuration
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin
-
-# Compilation flags
-export ARCHFLAGS="-arch x86_64"
-
-# SSH key path
-export SSH_KEY_PATH="~/.ssh/dsa_id"
-
-# MYSQL commands
-export PATH="$PATH:/usr/local/mysql/bin"
-
-# MYSQL commands
-export PATH="$PATH:/usr/local/mysql/bin"
-
-export LANG=en_US.UTF-8
-export EDITOR='nano'
-
-# Golang
-export GOPATH=~/gocode
-export PATH="$PATH:$GOPATH/bin"
-
-# gcloud
+# gcloud path completion
 source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
 source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 
-# iTerm2
+# iTerm2 shell integration
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
-# Yarn global modules bin
-export PATH="$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
-
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
-
-# Ensures that a specific version of node is used inside a specific folder
-if [[ -a $DOTFILES/node-version-adapter.zsh ]]; then
-  source $DOTFILES/node-version-adapter.zsh
-fi
---
+++ brew.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env sh
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install openssl
+brew install telnet
+brew install php
+brew install gmp
+brew install tmux
+brew install ssh-copy-id
+
+# tmux utils
+brew install tmux-mem-cpu-load
+brew install reattach-to-user-namespace
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install other useful binaries.
+brew install git
+brew install git-lfs
+brew install python@3
+brew install node
+brew install nvm # Manage multiple Node.js versions
+brew install ruby
+brew install rbenv # Ruby version manager
+brew install ack # Search tool like grep, but optimized for programmers
+brew install imagemagick --with-webp
+brew install tree # Display directories as trees (with optional color/HTML output)
+brew install rsync # Utility that provides fast incremental file transfer
+brew install insect # used for alfred's `clc` workflow.
+brew install m-cli # Swiss Army Knife for macOS
+brew install mysql
+brew install protobuf # Protocol buffers (Google's data interchange format)
+brew install httpie # User-friendly cURL replacement (command-line HTTP client)
+brew install youtube-dl # Download YouTube videos from the command-line
+brew install exercism
+brew install aspell # Spell checker with better logic than ispell
+brew install nginx
+brew install watch # Executes a program periodically, showing output fullscreen
+brew install fswatch # Monitor a directory for changes and run a shell command
+brew install redis
+brew install trash # CLI tool that moves files or folder to the trash
+brew install nmap # Port scanning utility for large networks
+brew install sqlite # Command-line interface for SQLite
+brew install htop # Improved top (interactive process viewer)
+brew install ffmpeg # Play, record, convert, and stream audio and video
+brew install wifi-password # Show the current WiFi network password
+brew install exiftool # Perl lib for reading and writing EXIF metadata
+brew install mkcert # Simple tool to make locally trusted development certificates
+brew install mitmproxy # Intercept, modify, replay, save HTTP/S traffic
+brew install hugo # Configurable static site generator
+brew install go # Go programming language
+brew install dep # Go dependency management tool
+brew install yarn # JavaScript package manager
+brew install mongodb-community # High-performance, schema-free, document-oriented database
+brew install jq # Lightweight and flexible command-line JSON processor
+
+# Remove outdated versions from the cellar.
+brew cleanup
