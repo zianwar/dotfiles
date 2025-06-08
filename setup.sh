879ded91ca8f974dae3ae757515251652f6fe21c
Anwar Ziani <hello@zianwar.com>
2025-06-07 17:44:12 -0700

    Update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 pushd "$HOME/.dotfiles" >/dev/null
 
 sudo apt install fzf eza --yes
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.dotfiles/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.dotfiles/oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.dotfiles/oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.dotfiles/oh-my-zsh/custom}/plugins/fzf-tab
 
 popd >/dev/null
